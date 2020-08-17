c9acc42dc8922d457f19980817c63aaafe278766
arellaTV <jeremiah.arella@gmail.com>
2020-08-16 22:37:02 -0700

    Populate schools in Filter results by menu

--- populate-filter-results-by.js
+++ populate-filter-results-by.js
@@ -1,17 +1,20 @@
 var preparationPrograms = ['Public', 'Private', 'Large', 'Medium', 'Small', 'Full-year clinical placements available', 'District-level partnership'];
+var schools = ['Public', 'Private', 'Charter', 'Large', 'Medium', 'Small', 'Elementary school', 'Middle school', 'High school', 'High-needs school', 'Hosts pre-clinical hours', 'Hosts culminating clinical placements', 'Hosts year-long clinical placements', 'Hosts enrichment programs', 'Professional development school'];
 
-var populatePreparationPrograms = function() {
-  var preparationProgramsContainer = document.getElementById('preparation-programs__collapse');
+var populateFilters = function(collapseId, filters) {
+  var collapse = document.getElementById(collapseId);
 
-  preparationPrograms.forEach(function(preparationProgram) {
-    var preparationProgramContainer = document.createElement('div');
-    var preparationProgramString = 
-     '<input type="checkbox" id="' + preparationProgram.toLowerCase() + '" value="' + preparationProgram + '"> ' +
-     '<label for="' + preparationProgram.toLowerCase() + '">' + preparationProgram + '</label>';
+  filters.forEach(function(filter) {
+    var filterContainer = document.createElement('div');
+    var filterString =
+      '<input type="checkbox" id="' + filter.toLowerCase() + '" value="' + filter + '"> ' +
+      '<label for="' + filter.toLowerCase() + '">' + filter + '</label>';
 
-     preparationProgramContainer.innerHTML = preparationProgramString;
-     preparationProgramsContainer.append(preparationProgramContainer);
+    filterContainer.innerHTML = filterString;
+    collapse.append(filterContainer);
   });
+
 }
 
-populatePreparationPrograms();
+populateFilters('preparation-programs__collapse', preparationPrograms);
+populateFilters('schools__collapse', schools);
