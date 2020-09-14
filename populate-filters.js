69905f16d284ed0a3253278701d8c71a50eeffad
Jay Arella <jeremiah.arella@gmail.com>
2020-09-14 04:39:31 -0700

    Fixes state toggles

--- populate-filters.js
+++ populate-filters.js
@@ -137,307 +137,307 @@ var states = [
     name: 'AL',
     label: 'Alabama',
     bbox: '-88.473227,30.223334,-84.88908,35.008028',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'AK',
     label: 'Alaska',
     bbox: '-179.148909,51.214183,-179.77847,71.365162',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'AZ',
     label: 'Arizona',
     bbox: '-114.81651,31.332177,-109.045223,37.00426',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'AR',
     label: 'Arkansas',
     bbox: '-94.617919,33.004106,-89.644395,36.4996',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'CA',
     label: 'California',
     bbox: '-124.409591,32.534156,-114.131211,42.009518',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'CO',
     label: 'Colorado',
     bbox: '-109.060253,36.992426,-102.041524,41.003444',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'CT',
     label: 'Connecticut',
     bbox: '-73.727775,40.980144,-71.786994,42.050587',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'DE',
     label: 'Delaware',
     bbox: '-75.788658,38.451013,-75.048939,39.839007',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'DC',
     label: 'District of Columbia',
     bbox: '-77.119759,38.791645,-76.909395,38.99511',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'FL',
     label: 'Florida',
     bbox: '-87.634938,24.523096,-80.031362,31.000888',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'GA',
     label: 'Georgia',
     bbox: '-85.605165,30.357851,-80.839729,35.000659',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'HI',
     label: 'Hawaii',
     bbox: '-178.334698,18.910361,-154.806773,28.402123',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'ID',
     label: 'Idaho',
     bbox: '-117.243027,41.988057,-111.043564,49.001146',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'IL',
     label: 'Illinois',
     bbox: '-91.513079,36.970298,-87.494756,42.508481',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'IN',
     label: 'Indiana',
     bbox: '-88.09776,37.771742,-84.784579,41.760592',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'IA',
     label: 'Iowa',
     bbox: '-96.639704,40.375501,-90.140061,43.501196',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'KS',
     label: 'Kansas',
     bbox: '-102.051744,36.993016,-94.588413,40.003162',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'KY',
     label: 'Kentucky',
     bbox: '-89.571509,36.497129,-81.964971,39.147458',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'LA',
     label: 'Louisiana',
     bbox: '-94.043147,28.928609,-88.817017,33.019457',
-    defaultValue: true
+    defaultValue: false
   },
   {
      name: 'ME',
      label: 'Maine',
      bbox: '-71.083924,42.977764,-66.949895,47.459686',
-    defaultValue: true
+    defaultValue: false
     },
   {
     name: 'MD',
     label: 'Maryland',
     bbox: '-79.487651,37.911717,-75.048939,39.723043',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'MA',
     label: 'Massachusetts',
     bbox: '-73.508142,41.237964,-69.928393,42.886589',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'MI',
     label:'Michigan',
     bbox: '-90.418136,41.696118,-82.413474,48.2388',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'MN',
     label: 'Minnesota',
     bbox: '-97.239209,43.499356,-89.491739,49.384358',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'MS',
     label: 'Mississippi',
     bbox: '-91.655009,30.173943,-88.097888,34.996052',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'MO',
     label: 'Missouri',
     bbox: '-95.774704,35.995683,-89.098843,40.61364',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'MT',
     label: 'Montana',
     bbox: '-116.050003,44.358221,-104.039138,49.00139',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NE',
     label: 'Nebraska',
     bbox: '-104.053514,39.999998,-95.30829,43.001708',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NV',
     label: 'Nevada',
     bbox: '-120.005746,35.001857,-114.039648,42.002207',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NH',
     label: 'New Hampshire',
     bbox: '-72.557247,42.69699,-70.610621,45.305476',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NJ',
     label: 'New Jersey',
     bbox: '-75.559614,38.928519,-73.893979,41.357423',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NM',
     label: 'New Mexico',
     bbox: '-109.050173,31.332301,-103.001964,37.000232',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NY',
     label: 'New York',
     bbox: '-79.762152,40.496103,-71.856214,45.01585',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'NC',
     label: 'North Carolina',
     bbox: '-84.321869,33.842316,-75.460621,36.588117',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'ND',
     label: 'North Dakota',
     bbox: '-104.0489,45.935054,-96.554507,49.000574',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'OH',
     label: 'Ohio',
     bbox: '-84.820159,38.403202,-80.518693,41.977523',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'OK',
     label: 'Oklahoma',
     bbox: '-103.002565,33.615833,-94.430662,37.002206',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'OR',
     label: 'Oregon',
     bbox: '-124.566244,41.991794,-116.463504,46.292035',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'PA',
     label: 'Pennsylvania',
     bbox: '-80.519891,39.7198,-74.689516,42.26986',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'RI',
     label: 'Rhode Island',
     bbox: '-71.862772,41.146339,-71.12057,42.018798',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'SC',
     label: 'South Carolina',
     bbox: '-83.35391,32.0346,-78.54203,35.215402',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'SD',
     label: 'South Dakota',
     bbox: '-104.057698,42.479635,-96.436589,45.94545',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'TN',
     label: 'Tennessee',
     bbox: '-90.310298,34.982972,-81.6469,36.678118',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'TX',
     label: 'Texas',
     bbox: '-106.645646,25.837377,-93.508292,36.500704',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'UT',
     label: 'Utah',
     bbox: '-114.052962,36.997968,-109.041058,42.001567',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'VT',
     label: 'Vermont',
     bbox: '-73.43774,42.726853,-71.464555,45.016659',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'VA',
     label: 'Virginia',
     bbox: '-83.675395,36.540738,-75.242266,39.466012',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'WA',
     label: 'Washington',
     bbox: '-124.763068,45.543541,-116.915989,49.002494',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'WV',
     label: 'West Virginia',
     bbox: '-82.644739,37.201483,-77.719519,40.638801',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'WI',
     label: 'Wisconsin',
     bbox: '-92.888114,42.491983,-86.805415,47.080621',
-    defaultValue: true
+    defaultValue: false
   },
   {
     name: 'WY',
     label: 'Wyoming',
     bbox: '-111.056888,40.994746,-104.05216,45.005904',
-    defaultValue: true
+    defaultValue: false
   }];
 
 var licensureAreas = [
